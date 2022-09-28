a8d3dac9fd47ffc604e3a259d2595fd3bd2a0304
Idham <idham16iramap@gmai.com>
2022-09-28 13:22:56 +0700

    backup laravel 8 damn bro

---
+++ README.md
@@ -0,0 +1,8 @@
+Cara Clone Project
+
+1. git clone
+2. Composer Install
+3. cp env.example env 
+4. php artisan key:generate
+7. Import database, ada di folder database
+6. last, php artisan serve
---
+++ routes/api.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
+
+//API route for register new user
+Route::post('/register', [App\Http\Controllers\API\AuthController::class, 'register']);
+//API route for login user
+Route::post('/login', [App\Http\Controllers\API\AuthController::class, 'login']);
+
+// Route::get('/login/{id}/{email}', function() {
+//     dd(Request::segment(1));
+// });
+
+//Protecting Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function(Request $request) {
+        return auth()->user();
+    });
+
+    // API route for logout user
+    Route::post('/logout', [App\Http\Controllers\API\AuthController::class, 'logout']);
+});
